75649f1ec4e71cf3f29153c9ee3c09aa9c1726a7
Willy <willysterr@gmail.com>
2019-12-09 15:03:10 +0100

    test

--- src/Entity/Admin.php
+++ src/Entity/Admin.php
@@ -2,79 +2,66 @@
 
 namespace App\Entity;
 
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\AdminRepository")
  */
-class Admin
-{
-    /**
-     * @ORM\Id()
-     * @ORM\GeneratedValue()
-     * @ORM\Column(type="integer")
-     */
-    private $id;
+class Admin {
+	/**
+	 * @ORM\Id()
+	 * @ORM\GeneratedValue()
+	 * @ORM\Column(type="integer")
+	 */
+	private $id;
 
-    /**
-     * @ORM\Column(type="string", length=255)
-     */
-    private $username;
+	/**
+	 * @ORM\Column(type="string", length=255)
+	 */
+	private $username;
 
-    /**
-     * @ORM\Column(type="string", length=255)
-     */
-    private $email;
+	/**
+	 * @ORM\Column(type="string", length=255)
+	 */
+	private $email;
 
-    /**
-     * @ORM\Column(type="string", length=255)
-     */
-    private $password;
+	/**
+	 * @ORM\Column(type="string", length=255)
+	 */
+	private $password;
 
+	public function getId():  ? int {
+		return $this->id;
+	}
 
-    public function getId(): ?int
-    {
-        return $this->id;
-    }
+	public function getUsername() :  ? string {
+		return $this->username;
+	}
 
-    public function getUsername(): ?string
-    {
-        return $this->username;
-    }
+	public function setUsername(string $username) : self{
+		$this->username = $username;
 
-    public function setUsername(string $username): self
-    {
-        $this->username = $username;
+		return $this;
+	}
 
-        return $this;
-    }
+	public function getEmail():  ? string {
+		return $this->email;
+	}
 
-    public function getEmail(): ?string
-    {
-        return $this->email;
-    }
+	public function setEmail(string $email) : self{
+		$this->email = $email;
 
-    public function setEmail(string $email): self
-    {
-        $this->email = $email;
+		return $this;
+	}
 
-        return $this;
-    }
+	public function getPassword():  ? string {
+		return $this->password;
+	}
 
-    public function getPassword(): ?string
-    {
-        return $this->password;
-    }
+	public function setPassword(string $password) : self{
+		$this->password = $password;
 
-    public function setPassword(string $password): self
-    {
-        $this->password = $password;
-
-        return $this;
-    }
-
-
+		return $this;
+	}
 
 }
